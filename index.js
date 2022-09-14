7a5669c74459065d07b32db9670ca8b26651ab07
Ugochrist13 <christianugoo@gmail.com>
2022-09-14 15:55:46 +0100

    updates

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ const slide = document.querySelector(".slide");
 const sunButn = document.querySelector(".sun")
 const moonButn = document.querySelector(".moon")
 
-let theme = true;
-// moonButn.style.display = "none"
 
-moonButn.addEventListener("click", function(conts) {
+let theme = false
+
+if(localStorage.getItem("themes") == null){
+    let data = JSON.stringify(theme)
+    localStorage.setItem("themes", data)
+}else{
+    let data = JSON.parse(localStorage.getItem("themes"))
+    theme = data
+}
+
+if (theme == true) {
+
     container.style.background = "#292036";
     container.style.border = "none"
     timeNBar.forEach(conts => {
@@ -44,13 +53,82 @@ moonButn.addEventListener("click", function(conts) {
     colButtns.forEach(conts => {
         conts.style.background = "#272833";
     })
-    slide.style.background = "#272833";
+    slide.style.background = "#272833"; 
+}else{ 
+    
+    container.style.background = "white";
+    container.style.border = "solid"
+    timeNBar.forEach(conts => {
+        conts.style.background = "white";
+        conts.style.color = "black";
+    })
+    lightCont.style.background = "lightgray";
+    // sunButn.style.display = "none"
+    sunButn.style.color = "gray";
+    sunButn.style.fontWeight = "100";
+    sunButn.style.transform = "translateY(0px)"
+    sunButn.style.fontSize = "1.1em";
+    // moonButn.style.display = "block"
+    moonButn.style.color = "black";
+    moonButn.style.transform = "translateY(-10px)"
+    moonButn.style.fontWeight = "900";
+    moonButn.style.fontSize = "1.3em";
+    inpNAns.forEach(conts => {
+        conts.style.color = "black";
+    })
+    grid.forEach(conts => {
+        conts.style.background = "lightgray";
+    })
+    buttns.forEach(conts => {
+        conts.style.background = "lightgray";
+        conts.style.color = "black";
+    })
+    colButtns.forEach(conts => {
+        conts.style.background = "lightgray";
+    })
+    slide.style.background = "lightgray";
+}
 
-    theme = true;
+moonButn.addEventListener("click", function(conts) {
+        container.style.background = "#292036";
+        container.style.border = "none"
+        timeNBar.forEach(conts => {
+            conts.style.background = "#272833";
+            conts.style.color = "white";
+        })
+        lightCont.style.background = "#272833";
+        sunButn.style.display = "block"
+        sunButn.style.color = "white";
+        sunButn.style.transform = "translateY(-10px)"
+        sunButn.style.fontWeight = "900";
+        sunButn.style.fontSize = "1.3em";
+        moonButn.style.color = "gray";
+        moonButn.style.transform = "translateY(0px)"
+        moonButn.style.fontWeight = "100";
+        moonButn.style.fontSize = "1.1em";
+        // moonButn.style.display = "none";
+        inpNAns.forEach(conts => {
+            conts.style.color = "white";
+        })
+        grid.forEach(conts => {
+            conts.style.background = "#272833";
+        })
+        buttns.forEach(conts => {
+            conts.style.background = "#272833";
+            conts.style.color = "white";
+        })
+        colButtns.forEach(conts => {
+            conts.style.background = "#272833";
+        })
+        slide.style.background = "#272833";
+    
+        theme = true;
+        data = JSON.stringify(theme)
+        localStorage.setItem("themes", data)
 });
 
 sunButn.addEventListener("click", function(conts) {
-    if (theme) {
+    if (theme == true) {
         container.style.background = "white";
         container.style.border = "solid"
         timeNBar.forEach(conts => {
@@ -84,8 +162,30 @@ sunButn.addEventListener("click", function(conts) {
         slide.style.background = "lightgray";
 
         theme = false;
+        data = JSON.stringify(theme)
+        localStorage.setItem("themes", data)
 
     }else{
         moonButn.addEventListener();
     }
 });
+
+
+// function display(val)
+// {
+//     const inpone = document.querySelector(".inp1");
+//     inpone.value += val
+//     const inptwo = document.querySelector(".inp2");
+//     inptwo.value = val
+//     const inpthree = document.querySelector(".inp3");
+//     inpthree.value += val
+//  }
+// function evaluate()
+// {
+//  const sol = document.querySelector(".ans")
+//  sol.value = eval(sol, "inpone.value,inptwo.value,inputhree.value")
+// }
+// function clr()
+// {
+//  document.getElementById("textval").value = ""
+// }
